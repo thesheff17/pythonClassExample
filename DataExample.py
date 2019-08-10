6802c9031c8b47bd64552cd7ea30d6f7350e5dd4
Dan Sheffner <Dan@Sheffner.com>
2019-08-09 20:07:16 -0500

    example on how to use python data classes

---
+++ DataExample.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2019, Dan Sheffner
+# All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+
+"""
+this will show you how to use python3.7+ data class
+"""
+
+# standard library imports
+import random
+import sys
+try:
+    from dataclasses import dataclass
+except ImportError:
+    msg1 = "sorry  your version of python is not high enough."
+    print(msg1)
+    print("python3.7 or higher is reqiured.")
+    sys.exit(1)
+
+# pip library imports
+
+# custom library imports
+
+@dataclass
+class Cars:
+    name: str
+    color: str
+    mpg: float
+    cost: float
+    tax_rate: float
+
+    def total_cost(self):
+        new_value = self.cost + (self.cost * self.tax_rate)
+        # how to format a float as currency with fstrings 
+        print(f'The cost with taxes for the {self.name} car will be ${new_value*1000:,.2f}.')
+
+
+if __name__ == "__main__":
+    # the first class
+    p1 = Cars('camery', 'red', 40, 10000.00, .07)
+    p2 = Cars('tesla', 'white', 0, 30000.00, .1)
+    p1.total_cost()
+    p2.total_cost()
